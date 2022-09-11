36c83ba26b1baf95803416e3a5444c4ca2e34fe0
srisarvesh r <r.srisarvesh@gmail.com>
2022-09-11 08:03:33 +0530

    After adding some comments on user routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,9 +2,12 @@ const userController =require("../controllers/user.controller");
 const {authJwt} = require("../middlewares/index");
 
 module.exports=(app)=>{
+    //So this method will retrive all the users of this application
     app.get("/crm/api/v1/users",[authJwt.verifyToken,authJwt.isAdmin],userController.findAll);
 
+    //And using this route we can retrive the user information of that particular userId which is passed.(And either admin or the owner of the id can only perform this).
     app.get("/crm/api/v1/users/:id",[authJwt.verifyToken,authJwt.isValidUserIdInReqParams,authJwt.isAdminOrOwner],userController.findByUserId);
 
+    //And either admin or the owner of the _id can update the user information.
     app.put("/crm/api/v1/users/:id",[authJwt.verifyToken,authJwt.isValidUserIdInReqParams,authJwt.isAdminOrOwner],userController.update)
 }
